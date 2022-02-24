2d0cbe14f7d10cb1669bccfa27cee615530012f6
Marcelo Torterolo <mtorterolo@switch.com.uy>
2022-02-24 13:45:59 -0300

    manifiestos para pvc

--- resourceMetadata/persistent-volume-claim.js
+++ resourceMetadata/persistent-volume-claim.js
@@ -9,50 +9,46 @@ module.exports = {
     },
     {
       type: 'input',
-      name: 'global-namespace',
-      message: 'The name of the namespace?',
-      default: '${NAMESPACE}',
-      validate: input => !!!input ? 'Please enter a value' : true,
-    },
-    {
-      type: 'input',
-      name: 'global-app-name',
-      message: 'The name of the app (if any)?',
-      default: '${CI_PROJECT_PATH_SLUG}',
-    },
-    {
-      type: 'input',
-      name: 'global-app-tier',
-      message: 'The tier name of the app (if any)?',
-    },
-    {
-      type: 'input',
       name: 'pvc-name',
       message: 'The name of the volume claim?',
-      default: '${CI_PROJECT_PATH_SLUG}',
     },
     {
       type: 'checkbox',
-      name: 'pvc-access-modes',
-      message: 'Which access modes?',
+      name: 'pvc-access-mode',
+      message: 'Which access mode?',
       choices: ['ReadWriteOnce', 'ReadOnlyMany', 'ReadWriteMany'],
-      default: ['ReadWriteOnce'],
-      validate: input => input.length > 0 ? true : 'Please select at least one.',
+      default: ['ReadWriteMany'],
+      validate: input => input.length === 1 ? true : 'Please select one.',
+    },
+    {
+      type: 'confirm',
+      name: 'pvc-db',
+      message: 'It PVC for database?',
+      default: false,
+    },
+    {
+      type: 'confirm',
+      name: 'pvc-server',
+      message: 'It PVC for server (backend)?',
+      default: false,
     },
     {
       type: 'input',
       name: 'pvc-size',
       message: 'How big should the PVC be?',
-      default: '5Gi',
+      default: '500Mi',
       validate: input => !!!input ? 'Please enter a value' : true,
     },
   ],
   transformAnswers(data) {
-    if (typeof data['global-app-name'] === 'string') {
-      data['global-app-name'] = data['global-app-name'].trim();
+    if (typeof data['global-name'] === 'string') {
+      data['global-name'] = data['global-name'].trim();
     }
-    if (typeof data['global-app-tier'] === 'string') {
-      data['global-app-tier'] = data['global-app-tier'].trim();
+    if (typeof data['pvc-name'] === 'string') {
+      data['pvc-name'] = data['pvc-name'].trim();
+    }
+    if (typeof data['pvc-size'] === 'string') {
+      data['pvc-size'] = data['pvc-size'].trim();
     }
   },
 };
